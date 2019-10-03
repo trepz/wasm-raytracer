45c0066820a55649e89cccc35de82f57bebf3f39
Robert Worman <tr3ppy@gmail.com>
2019-10-03 20:34:51 +1000

    Get rust to render directly to canvas

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::Clamped;
+use web_sys::{CanvasRenderingContext2d, ImageData};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,11 +11,25 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub fn render(
+    ctx: &CanvasRenderingContext2d,
+    width: u32,
+    height: u32,
+) -> Result<(), JsValue> {
+    let mut pixels = get_pixel_values(width, height);
+
+    let data = ImageData::new_with_u8_clamped_array_and_sh(
+        Clamped(&mut pixels),
+        width,
+        height
+    )?;
+    ctx.put_image_data(&data, 0.0, 0.0)
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-raytracer!");
+fn get_pixel_values(width: u32, height: u32) -> Vec<u8> {
+    let pixels = (0..width * height)
+        .map(|_| { vec![255, 0, 0, 255] })
+        .flatten()
+        .collect();
+    pixels
 }
