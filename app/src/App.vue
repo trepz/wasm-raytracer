4afd79832c164cdd9cc1cadca00ad9577a3f13de
Robert Worman <tr3ppy@gmail.com>
2019-10-03 19:07:32 +1000

    Add a canvas with junk image data

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    wasm raytracer
+    <h2>wasm raytracer</h2>
+    <div class="canvas-container">
+      <canvas width="300" height="200" ref="canvas"></canvas>
+    </div>
   </div>
 </template>
 
@@ -10,8 +13,19 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component
 export default class App extends Vue {
   async mounted() {
-    const wasm = await import('wasm-raytracer');
-    wasm.greet();
+    const wasm = await import("wasm-raytracer");
+
+    const c = <HTMLCanvasElement>this.$refs["canvas"];
+    const ctx = c.getContext("2d");
+    const imgData = ctx.createImageData(300, 200);
+    let i;
+    for (i = 0; i < imgData.data.length; i += 4) {
+      imgData.data[i+0] = 255;
+      imgData.data[i+1] = 0;
+      imgData.data[i+2] = 0;
+      imgData.data[i+3] = 255 - (i / 800);
+    }
+    ctx.putImageData(imgData, 0, 0);
   }
 }
 </script>
