10fb39488406f3553063afc8df5e774d552c1551
Robert Worman <tr3ppy@gmail.com>
2019-10-06 15:04:14 +1000

    Run rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -11,24 +11,16 @@ use web_sys::{CanvasRenderingContext2d, ImageData};
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn render(
-    ctx: &CanvasRenderingContext2d,
-    width: u32,
-    height: u32,
-) -> Result<(), JsValue> {
+pub fn render(ctx: &CanvasRenderingContext2d, width: u32, height: u32) -> Result<(), JsValue> {
     let mut pixels = get_pixel_values(width, height);
 
-    let data = ImageData::new_with_u8_clamped_array_and_sh(
-        Clamped(&mut pixels),
-        width,
-        height
-    )?;
+    let data = ImageData::new_with_u8_clamped_array_and_sh(Clamped(&mut pixels), width, height)?;
     ctx.put_image_data(&data, 0.0, 0.0)
 }
 
 fn get_pixel_values(width: u32, height: u32) -> Vec<u8> {
     let pixels = (0..width * height)
-        .map(|_| { vec![255, 0, 0, 255] })
+        .map(|_| vec![255, 0, 0, 255])
         .flatten()
         .collect();
     pixels
